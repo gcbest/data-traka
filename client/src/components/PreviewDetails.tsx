5be7230bb18eaa90f3c05e84d88fea1409278b04
Godfrey Best <godfrey.c.best@gmail.com>
2020-11-07 10:15:54 -0500

    start stock preview

---
+++ client/src/components/PreviewDetails.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react'
+
+interface Props {
+
+}
+
+const PreviewDetails: React.FC<Props> = () => {
+    return (
+        <Box>
+            <p className="name"></p>
+            <p className="price"></p>
+        </Box>
+    );
+}
+
+export default PreviewDetails;
---
+++ client/src/components/StockPreview.tsx
@@ -0,0 +1,18 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react'
+import PreviewDetails from './PreviewDetails';
+
+interface Props {
+
+}
+
+const StockPreview: React.FC<Props> = () => {
+    return (
+        <Box borderWidth="1px" borderRadius="lg" display="flex">
+            <img src="" alt=""/>
+            <PreviewDetails />
+        </Box>
+    );
+}
+
+export default StockPreview;
