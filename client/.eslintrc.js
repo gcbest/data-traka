5be896d9b37f80e75bd1631f7633201eb2a7187e
Godfrey Best <godfrey.c.best@gmail.com>
2020-11-16 00:36:44 -0500

    comment out eslint for heroku

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,23 @@
-module.exports = {
-  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  rules: {
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
-    '@typescript-eslint/indent': [2, 2],
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'import/extensions': [1, 'never'],
-    'max-len': [1, { code: 120 }],
-    'react/prop-types': [0, { }],
-  },
-};
+// module.exports = {
+//   extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['@typescript-eslint'],
+//   settings: {
+//     'import/parsers': {
+//       '@typescript-eslint/parser': ['.ts', '.tsx'],
+//     },
+//     'import/resolver': {
+//       typescript: {},
+//     },
+//   },
+//   rules: {
+//     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+//     'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+//     '@typescript-eslint/indent': [2, 2],
+//     'no-use-before-define': 'off',
+//     '@typescript-eslint/no-use-before-define': ['error'],
+//     'import/extensions': [1, 'never'],
+//     'max-len': [1, { code: 120 }],
+//     'react/prop-types': [0, { }],
+//   },
+// };
